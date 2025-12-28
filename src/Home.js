0f4e9211ced2544dc5f76fcb5a3b3a1312844404
2shoneycom <2shoneycom@gmail.com>
2025-12-28 17:35:42 +0900

    Responsive Web Design
    Work On:
    -Desktop/Tablet/Mobile
    
    To do:
    -scss recode

--- src/Home.js
+++ src/Home.js
@@ -6,35 +6,35 @@ const heroDataSample = [
     id: 1,
     title: "Dialogue Boost: How Amazon is using AI to enhance TV and movie dialogue",
     desc: "New audio-processing technology is making entertainment more accessible for millions of viewers.",
-    image: "/images/slide1.jpg", // 이미지 경로
+    image: "/images/1764166800383.jpeg", // 이미지 경로
     link: "/post/1" // 'Read more' 버튼 누르면 갈 주소
   },
   {
     id: 2,
     title: "The overthinking problem in AI",
     desc: "Reasoning models can generate seven to 10 times as many tokens as necessary on simple tasks, creating unsustainable costs at scale. Amazon's vision for metacognitive AI could fundamentally shift how models allocate computational resources.",
-    image: "/images/slide2.jpg",
+    image: "/images/1764166800383.jpeg",
     link: "/post/2"
   },
   {
     id: 3,
     title: "Nova Forge: Build your own frontier AI",
     desc: "AGI SVP Rohit Prasad on how to mix your data with Amazon's training data at every stage — deep customization without catastrophic forgetting.",
-    image: "/images/slide3.jpg",
+    image: "/images/1764166800383.jpeg",
     link: "/post/3"
   },
   {
     id: 4,
     title: "Demystifying AI agents",
     desc: "How agentic systems work under the hood — and how AWS’s new AgentCore framework implements their essential components.",
-    image: "/images/slide3.jpg",
+    image: "/images/1764166800383.jpeg",
     link: "/post/3"
   },
   {
     id: 5,
     title: "How Amazon uses AI agents to anticipate and counter cyber threats",
     desc: "Competitive-agent architecture develops security protections at machine speed, reducing weeks of work to hours.",
-    image: "/images/slide3.jpg",
+    image: "/images/1764166800383.jpeg",
     link: "/post/3"
   },
 ];
